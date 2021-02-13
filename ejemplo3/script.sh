b008ff97a81e88c578f3dd2eca9d497c225c92ef
José Domingo Muñoz <josedom24@gmail.com>
2021-02-13 21:11:52 +0100

    ejemplo3

--- ejemplo3/script.sh
+++ ejemplo3/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/ServerName/ServerName $SERVER_NAME/g" /etc/apache2/sites-available/000-default.conf 
+sed -i "s/#ServerName www.example.com/ServerName $SERVER_NAME/g" /etc/apache2/sites-available/000-default.conf 
 apache2ctl -D FOREGROUND
